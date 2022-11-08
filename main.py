eeafac4c1b84672ef5c58c82952f19e15708f405
Yury Vereskun <satriks@mail.ru>
2022-11-08 20:16:48 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import json
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+from models import craate_tables, Publisher,Shop,Book,Stock,Sale
+
+'''Скрипт подключается к БД любого типа на ваш выбор.'''
+
+DSN = 'postgresql://postgres:123st321@localhost:5432/netology_Alchemy'
+engine = sqlalchemy.create_engine(DSN)
+
+craate_tables(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+'''Скрипт импортирует необходимые модели данных '''
+
+model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale
+        }
+
+with open('tests_data.json', 'r') as file:
+    data = json.load(file)
+
+    for record in data:
+        session.add(model[record.get('model')](id= record.get('pk'), **record.get('fields')))
+        session.commit()
+
+''' Скрипт который выводит название магазина, где продается издатель (по id или имени'''
+
+quer = input('Введите id или имя издателя :')
+answer = []
+print()
+print('Издатель продается в магазинах :', end=' ')
+
+if quer.isdigit():
+    for resalt in session.query(Publisher, Shop).join(Book).join(Stock).join(Shop).filter(Publisher.id == quer).all():
+        print(resalt[-1].name, end='; ')
+else:
+    for resalt in session.query(Publisher, Shop).join(Book).join(Stock).join(Shop).filter(Publisher.name == quer).all():
+        print(resalt[-1].name, end='; ')
+print()
+session.close()
---
+++ models.py
@@ -0,0 +1,59 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Publisher(Base):
+    __tablename__ = 'publisher'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=60), unique=True)
+
+    def __str__(self):
+        return f'Course {self.id}: {self.name}'
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    title = sq.Column(sq.String(length=100),  nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
+
+    publishers = relationship(Publisher, backref='books')
+
+    def __str__(self):
+        return f'Course {self.id}: {self.title}:{self.id_publisher}'
+
+class Shop(Base):
+    __tablename__ = 'shop'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=50), nullable = False)
+
+class Stock(Base):
+    __tablename__ = "stock"
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey('shop.id'))
+    id_book = sq.Column(sq.Integer, sq.ForeignKey('book.id'))
+    count = sq.Column(sq.Integer, )
+
+    bookss = relationship(Book, backref='stocks')
+    shops = relationship(Shop, backref='stocks2')
+
+
+
+class Sale(Base):
+    __tablename__ = 'sale'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    price = sq.Column(sq.Float)
+    date_sale = sq.Column(sq.DATE)
+    count = sq.Column(sq.Integer)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey('stock.id'))
+
+    stocks = relationship(Stock, backref='sales')
+
+def craate_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
